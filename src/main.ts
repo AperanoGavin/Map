52f65dc0b7d464d1e326688f438761aad85f4453
Gavin APERANO MOULOUNGUI <gavin.aperano-mouloungui@ekino.com>
2024-04-04 14:13:39 +0200

    start

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,85 @@ let currentPopup: any = undefined;
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
+    //WA.state.saveVariable(key : string, data : unknown): void
+
+    //parcouri les joueurs et compter leur role si c'est un comédien faire +1
+    let currentComedian = 0;
+    let currentAudience = 0;
+    const players = WA.players.list();
+
+
+
+    for (const player of players) {
+        if(player.state.role === "comedian"){
+            currentComedian++;
+        }
+        else if(player.state.role === "audience"){
+            currentAudience++;
+        }
+    }
+    //demander pour recuperer toutes les variables de tous les joueurs
+
+    //si un joueur change sa variable role alors on met à jour le compteur de comédien et d'audience 
+   /*  WA.state.onVariableChange('role').subscribe((value) => {
+        if(value === "comedian"){
+            currentComedian++;
+        }
+        else if(value === "audience"){
+            currentAudience++;
+        }
+    }); */
+
+
+    console.log(currentComedian)
+    console.log(WA.player.state.role)
+    if(currentComedian<=0 || (WA.player.state.role === "comedian" || WA.player.state.role === "audience")){
+
+
+        WA.ui.openPopup("chooseRole", "Choose your role", [
+            {
+                label: "Comedian",
+                className: "primary",
+                callback: (popup) => {
+                    WA.player.state.saveVariable("role", "comedian", {
+                        public: true,
+                        persist: true,
+                        ttl: 24 * 3600,
+                        scope: "world",
+                    });
+                    currentComedian++;
+                    console.log(currentComedian)
+                    console.log(currentAudience)
+                    popup.close()
+                }
+            },
+            {
+                label: "Audience",
+                className: "primary",
+                callback: (popup) => {
+                    WA.player.state.saveVariable("role", "audience", {
+                        public: true,
+                        persist: true,
+                        ttl: 24 * 3600,
+                        scope: "world",
+                    });
+                    currentAudience++;
+                    popup.close()
+                }
+            }
+        ]) 
+    }
+
+    WA.player.state.saveVariable("role", "audience", {
+        public: true,
+        persist: true,
+        ttl: 24 * 3600,
+        scope: "world",
+    });
+
+
+   
+        
 
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
@@ -25,6 +104,7 @@ WA.onInit().then(() => {
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
+
 
 function closePopup(){
     if (currentPopup !== undefined) {
